f889057b8b80c930d39e7aff3292aea8a52582a4
msAlexG <msgrishkov@gmail.com>
2022-03-14 12:15:25 +0300

    first commit

--- lib/pages/superhero_page.dart
+++ lib/pages/superhero_page.dart
@@ -5,33 +5,38 @@ import 'package:superheroes/widgets/action_button.dart';
 
 class SuperheroPage extends StatelessWidget {
   String name;
-   SuperheroPage({
-    Key? key,
-    required this.name
-  }) : super(key: key);
 
+  SuperheroPage({Key? key, required this.name}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
+    return Scaffold(
       backgroundColor: SuperheroesColors.background,
-      body: Stack(
-        children: [
-          Center(child: Text(name,style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600, color: SuperheroesColors.white),)),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: ActionButton(text: 'Back', onTap: () {
+      body: SafeArea(
+        child: Stack(
+    children: [
+        Center(
+            child: Text(
+          name,
+          style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+              color: SuperheroesColors.white),
+        )),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: ActionButton(
+            text: 'Back',
+            onTap: () {
               Navigator.of(context).push(MaterialPageRoute(
                 builder: (context) => MainPage(),
               ));
-            }
-              ,),
-          )
-
-        ],
-
+            },
+          ),
+        )
+    ],
+        ),
       ),
-    ));
-
+    );
   }
 }
