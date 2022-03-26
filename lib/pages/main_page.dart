b659c1fd76afbd942c15291045d0a76337f54c55
msAlexG <99503127+msAlexG@users.noreply.github.com>
2022-03-26 19:10:25 +0300

    Update main_page.dart

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -6,6 +6,7 @@ import 'package:superheroes/blocs/main_bloc.dart';
 import 'package:superheroes/pages/superhero_page.dart';
 import 'package:superheroes/resources/superheroes_colors.dart';
 import 'package:superheroes/resources/superheroes_images.dart';
+import 'package:superheroes/widgets/action_button.dart';
 import 'package:superheroes/widgets/info_with_button.dart';
 import 'package:superheroes/widgets/superhero_card.dart';
 
